1c90dbf87c87cd65dfbbd23a721505b4562b79f7
dannycornelio <licdannycornelio@gmail.com>
2023-05-16 15:47:35 -0600

    index guardado uso de try

--- conexion.php
+++ conexion.php
@@ -1,12 +1,16 @@
 <?php
-try{
-$conexion = new PDO("informix:host=host.docker.internal; service=9088;database=tienda; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix");
-}catch(PDOException $e){
-    echo "Error: " . $e->getMessage();
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+try {
+    $conexion = new PDO("informix:host=host.docker.internal; service=9088;database=mdn_cornelio; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix");
+    echo "conexión exitosa";
+} catch (PDOException $e) {
+    echo "No se pudo conectar";
+    echo "<br>";
+    echo $e->getMessage();
 }
- echo "<br>";
- echo "Mensaje despues de la conexion";
-
+echo "<br>";
+echo "mensaje despues de la conexión";
 
 ?>
